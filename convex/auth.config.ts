6f574f63a422a5cee793d78bb9f1b32d7b6dde73
theodore-12 <theodore.414476@nv.ccsd.net>
2026-04-27 11:40:03 -0700

    update

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,12 +1,5 @@
 import type { AuthConfig } from 'convex/server'
 
-console.log('[auth.config] CLERK_JWT_ISSUER_DOMAIN:', process.env.CLERK_JWT_ISSUER_DOMAIN)
-
 export default {
-  providers: [
-    {
-      domain: process.env.CLERK_JWT_ISSUER_DOMAIN!,
-      applicationID: 'convex',
-    },
-  ],
+  providers: [],
 } satisfies AuthConfig
